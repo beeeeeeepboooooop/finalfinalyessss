3467c9b9b6ed35ae3a95baee1fb5ea1452bc80db
beeeeeeepboooooop <h.khalid.alnaqbi@gmail.com>
2025-05-08 21:54:18 +0400

    Delete grand_prix_shared_path.py

--- grand_prix_shared_path.py
+++
@@ -1,6 +0,0 @@
-import os
-
-SHARED_DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "shared_data")
-
-if not os.path.exists(SHARED_DATA_PATH):
-    os.makedirs(SHARED_DATA_PATH)
